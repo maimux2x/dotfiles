df3d278cabf50ac2e6b6144b56fc878c73f2a017
maimux2x <m0smile10often@gmail.com>
2025-09-10 13:55:33 +0900

    update

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,20 @@
 return {
   'neovim/nvim-lspconfig',
   config = function()
+    local lspconfig = require('lspconfig')
+
+    lspconfig.ruby_lsp.setup({
+      settings = {
+        rubyLsp = {
+          addonSettings = {
+            ['Ruby LSP Rails'] = {
+              enablePendingMigrationsPrompt = false
+            }
+          }
+        }
+      }
+    })
+
     vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
